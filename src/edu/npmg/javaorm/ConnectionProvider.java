e25c886e5d5b37a41f8ef9006b737943155a812f
georgy-kirilov <georgynk17@gmail.com>
2021-11-25 11:24:03 +0200

    Remove my stuff

--- src/edu/npmg/javaorm/ConnectionProvider.java
+++
@@ -1,9 +0,0 @@
-package edu.npmg.javaorm;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionProvider
-{
-	Connection createConnection() throws SQLException;
-}
--- src/edu/npmg/javaorm/QueryBuilder.java
+++
@@ -1,6 +0,0 @@
-package edu.npmg.javaorm;
-
-public class QueryBuilder
-{
-
-}
